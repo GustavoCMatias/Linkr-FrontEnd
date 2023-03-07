ac93a527193588165a0de6ed05bac68e30473ff6
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-03-07 18:57:50 -0300

    fix: route

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,5 +1,46 @@
+import {SignInContainer, Title,  FormContainer, TextContainer} from './SignInPageCss.js';
+
 export default function SignInPage() {
     return (
-       <></>
+        <SignInContainer>
+
+        <Title>
+            linkr
+            <p>save, share and discover
+                <br />
+                the best links on the web</p>
+        </Title>
+
+        <FormContainer>
+            <form onSubmit={createAccount}>
+                <input
+                    id='email'
+                    type='text'
+                    value={form.email}
+                    placeholder='email'
+                    onChange={handleForm}
+                    required
+                />
+
+                <input
+                    id='password'
+                    type='password'
+                    value={form.password}
+                    placeholder='password'
+                    onChange={handleForm}
+                    required
+                />
+
+                <button type='submit' >Sign Up</button>
+            </form>
+
+            <TextContainer>
+                <Link to='/register'>
+                    <h3>First time? Create an account!</h3>
+                </Link>
+            </TextContainer>
+        </FormContainer>
+
+    </SignInContainer >
     )
 }
