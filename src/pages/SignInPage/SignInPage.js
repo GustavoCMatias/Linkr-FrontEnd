7833bc1b239eb5c8afbb7fcc79bdea23a9688344
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-03-07 21:19:21 -0300

    feat: signIn layout done

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,46 +1,70 @@
-import {SignInContainer, Title,  FormContainer, TextContainer} from './SignInPageCss.js';
+import { SignInContainer, Title, FormContainer, TextContainer } from './SignInPageCss.js';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import apiAuth from '../../services/apiAuth.js';
+
 
 export default function SignInPage() {
+
+    const navigate = useNavigate();
+    //const { setUser } = useContext(UserContext);
+
+    const [form, setForm] = useState({ email: "", password: "" });
+
+    function signIn(e) {
+        e.preventDefault();
+
+    apiAuth
+      .singIn(form)
+      .then((res) => {
+        console.log(res.data);
+        //setUser(res.data);
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+    }
+
+    function handleForm(e) {
+        setForm({ ...form, [e.target.id]: e.target.value });
+      }
     return (
         <SignInContainer>
+            <Title>
+                linkr
+                <p>save, share and discover
+                    <br />
+                    the best links on the web</p>
+            </Title>
 
-        <Title>
-            linkr
-            <p>save, share and discover
-                <br />
-                the best links on the web</p>
-        </Title>
+            <FormContainer>
+                <form onSubmit={signIn}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        value={form.email}
+                        onChange={handleForm}
+                    />
 
-        <FormContainer>
-            <form onSubmit={createAccount}>
-                <input
-                    id='email'
-                    type='text'
-                    value={form.email}
-                    placeholder='email'
-                    onChange={handleForm}
-                    required
-                />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        value={form.password}
+                        onChange={handleForm}
+                    />
 
-                <input
-                    id='password'
-                    type='password'
-                    value={form.password}
-                    placeholder='password'
-                    onChange={handleForm}
-                    required
-                />
+                    <button type="submit">Sign In</button>
+                </form>
 
-                <button type='submit' >Sign Up</button>
-            </form>
-
-            <TextContainer>
-                <Link to='/register'>
-                    <h3>First time? Create an account!</h3>
-                </Link>
-            </TextContainer>
-        </FormContainer>
-
-    </SignInContainer >
-    )
+                <TextContainer>
+                    <Link to="/sign-up">
+                        <h3>First time? Create an account!</h3>
+                    </Link>
+                </TextContainer>
+            </FormContainer>
+        </SignInContainer>
+    );
 }
